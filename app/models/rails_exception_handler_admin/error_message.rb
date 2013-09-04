c0c6b1e063a636c5eaaf297aeffed7196cf31362
Matt W <uranazo@gmail.com>
2013-09-03 23:39:57 -0400

    Initial bootstrap setup to read error messages

--- app/models/rails_exception_handler_admin/error_message.rb
+++ app/models/rails_exception_handler_admin/error_message.rb
@@ -1,6 +1,8 @@
 module RailsExceptionHandlerAdmin
   class ErrorMessage < ActiveRecord::Base
-    set_table_name :error_messages
     attr_accessible :app_name, :class_name, :created_at, :doc_root, :message, :params, :referer_url, :target_url, :trace, :updated_at, :user_agent, :user_info
+    # Enforced by rails_exception_handler gem
+    set_table_name :error_messages
+    establish_connection "exception_database"
   end
 end
